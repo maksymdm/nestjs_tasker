ac581c39cb9958f958f0545f2849942c5264ca54
maximdm <maximcodm@yahoo.com>
2020-07-30 12:21:22 +0300

    add validation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 
 import * as uuid from 'uuid';
@@ -46,16 +46,20 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+        if(!found) {
+            throw new NotFoundException(`Task with id "${id}" not found`);
+        }
+        return found
     }
 
     deleteTask(id: string): void {
-        const index = this.tasks.findIndex(task => task.id === id);
-        if(index >= 0) this.tasks.splice(index, 1);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id === found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
-        const task = this.tasks.find(task => task.id === id);
+        const task = this.getTaskById(id);
         task.status = status;
         return task;
     }
