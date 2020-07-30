29ec4c5f0dbd0c92cccb65ee9314261b30f3eb41
maximdm <maximcodm@yahoo.com>
2020-07-30 11:46:37 +0300

    add crud endpoints

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+
+import * as uuid from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +11,52 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTasksWithFilers(filterDto: GetTasksFilterDto): Task[] {
+        const {status, search} = filterDto;
+        let tasks = this.getAllTasks();
+
+        if(status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+
+        if(search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search)
+            );
+        }
+
+        return this.tasks;
+    }
+
+    createTask(createTaskDto:CreateTaskDto) {
+        const {title, description} = createTaskDto;
+
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status:TaskStatus.OPEN
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    deleteTask(id: string): void {
+        const index = this.tasks.findIndex(task => task.id === id);
+        if(index >= 0) this.tasks.splice(index, 1);
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.tasks.find(task => task.id === id);
+        task.status = status;
+        return task;
     }
 }
